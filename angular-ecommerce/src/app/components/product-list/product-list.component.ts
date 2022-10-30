44face2c329a5c256586ace6858013b2ed9f9f9e
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-30 17:07:38 -0400

    End release 1.0

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,29 @@
+import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  //Post construct
   ngOnInit(): void {
+    this.listProduct();
+  }
+  listProduct() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 
 }
