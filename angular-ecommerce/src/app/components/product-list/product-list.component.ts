a6f0b89cb51ced42904ecd8366342f88438d3ec3
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-30 23:19:19 -0400

    routes

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,40 @@
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  //templateUrl: './product-list-table.component.html',
-  //templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+     //ActivatedRoute -> get the 'id' parameter
+   }
 
   //Post construct
   ngOnInit(): void {
-    this.listProduct();
+    this.route.paramMap.subscribe(() => {
+      this.listProduct();
+    });
   }
+
   listProduct() {
-    this.productService.getProductList().subscribe(
+    //check id parameter is available
+    const hasCategory: boolean = this.route.snapshot.paramMap.has('id'); // id comes from placeholder from path ex: category/1
+    if(hasCategory) {
+      //convert string to number -> +
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!; // !  at the end is non-null assertion operator, tell compiler this object is not null
+    } else {
+      //value set by default
+      this.currentCategoryId = 1;
+    }
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
